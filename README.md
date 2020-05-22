92ac82aacb67a670f6ae4ca6b52594d073a585dc
Demitri Haddad <demitrih@gmail.com>
2020-05-22 15:22:28 -0600

    files

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # dining-web-project
 Interview Project for Applicants
+
+
+# Dining Web Project
+
+This project is to create a basic website from scratch that matches the BYU Dining Services theme. The purpose of this is to test your basic HTML, CSS, and JavaScript skills. Please do not spend more than 3 hours on this project. 
+
+
+### Project Description
+
+Create a dynamic page for E.A.T. that changes content based on what the user clicks. See photos for details. 
+
+### Requirements:
+
+  - BYU header with links (http://dining.byu.edu/styleguide/header.html)
+  - Change the top banner image based on the food that is clicked. Corresponding banners will be found in the images folder. 
+  - Change the `.eat-description` based on the class that is clicked. 
+  - BYU footer with links (http://dining.byu.edu/styleguide/footer.html)
+  - BONUS: if you can make it mobile friendly that changes `.eat-container` to `display: block;` when the page shrinks below 800px. 
+
+
+### Tech
+You are limited to the following languages: 
+* HTML
+* CSS
+* JavaScript (jQuery is okay)
+
+
+
+
+
+
+
+ 
---
+++ scripts/eat.js
@@ -0,0 +1,54 @@
+/**
+ * @file
+ * All Javascript for EAT page
+ */
+
+/**
+ * Section 1: Header Menu
+ * Behavior for the menu bar at the header
+ *
+ * Sections:
+ * 1.1) Controller for clicking on menu options
+ */
+$(document).ready(function () {
+    "use strict";
+
+
+    /**
+     * ChooseMyPlate Controller
+     * Behavior for choosing different options on the ChooseMyPlate image
+     *
+     * Requirements:     
+     * When you click on any section of the SVG, the background color of the .eat-ribbon class will change to the corresponding color. 
+     * For example if you click vegetables, color of .eat-ribbon will change to #C13937
+     * 
+     * It will also clear whatever text is currently in the .eat-description class and will replace it with the text associated with the class. 
+     * For example, when first visiting the page, when I click on vegetables it will diplay 
+     * 
+     * 
+     * Color key:
+     * Fruits: #C13937
+     * Grains: #EC8533
+     * Vegetables: #C1D15A
+     * Proteins: #544986
+     * 
+     */    
+   
+    $("#vegetables").click(function () {
+
+    });
+
+    $("#fruits").click(function () {
+
+    });
+
+    $("#grains").click(function () {
+
+    });
+
+    $("#proteins").click(function () {
+
+    });
+
+   
+});
